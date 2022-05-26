7d8497c5401a18bcb49e4346b4e6831fea1d0aa5
Walessonrdreis <wallllase@hotmail.com>
2022-05-26 16:38:10 -0300

    Commit aula: add pasta do exemplo da aula springboot2 inicila commit

---
+++ demoaulaspringboot2/src/main/java/com/example/aula/springboot2/demoaulaspringboot2/Demoaulaspringboot2Application.java
@@ -0,0 +1,13 @@
+package com.example.aula.springboot2.demoaulaspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoaulaspringboot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoaulaspringboot2Application.class, args);
+	}
+
+}
---
+++ demoaulaspringboot2/src/test/java/com/example/aula/springboot2/demoaulaspringboot2/Demoaulaspringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aula.springboot2.demoaulaspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoaulaspringboot2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
